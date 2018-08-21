22630d7b1ebe4ef1a6f25ef3d203001123a4286d
Joey Cho <cjy12182000@naver.com>
2018-08-21 00:47:59 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,24 @@
 require 'pry'
 class Show < ActiveRecord::Base
-  binding.pry
+  def highest_rating
+  end
+
+  def most_popular_show
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
+
+  def ratings_sum
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
+  
 end
